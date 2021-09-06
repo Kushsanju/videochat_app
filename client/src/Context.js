46da846d664de5306ea38055037ae06399d69198
Sanjana Kushwaha <gssanjanakush@gmail.com>
2021-09-06 11:24:28 +0530

    first commit

--- client/src/Context.js
+++ client/src/Context.js
@@ -7,7 +7,7 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+const socket = io("https://video-call-app-san.herokuapp.com/");
 
 
 const ContextProvider = ({ children }) => {
